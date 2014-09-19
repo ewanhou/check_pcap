8d1e07bdfdf37cb57bc49c43abc2236dd9f1acf4
Ewan <ewan_hou@gemteks.com>
2014-09-19 11:22:27 +0800

    modify usage

--- main.c
+++ main.c
@@ -4,11 +4,22 @@
 #include <netinet/ip.h>
 #include <arpa/inet.h>
 #include <pcap.h>
-
 #include "pkt_buff.h"
 #include "ether_proto.h"
 #include "utils.h"
 
+void usage(){
+	printf("Usage: check_pcap <file> ip <src_ip> <des_ip>\n");
+	printf("       check_pcap <file> statistic\n\n");
+
+}
+
+int isValidIpAddress(char *ipAddress)
+{
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
+    return result != 0;
+}
 
 int main(int argc, char **argv)
 {
@@ -17,10 +28,10 @@ int main(int argc, char **argv)
 	int i = 0, link_type;
 
 	if (!(argc >= 3)) {
-		printf("Usage: check_pcap <file>\n");
+		usage();
 		exit(1);
 	}  else if(argc > 5) {
-                printf("Usage: check_pacp <file>\n");
+		usage();
                 exit(1);
 	}
 
@@ -28,8 +39,15 @@ int main(int argc, char **argv)
 		printf("DO STATISTIC\n");
 	} else if(strcmp(argv[2], "ip") == 0) {
 		printf("DO IP query\n");
+		int ret_1 = isValidIpAddress(argv[3]);
+		int ret_2 = isValidIpAddress(argv[4]);
+		if(ret_1 == 0 || ret_2 == 0) {
+			printf("Wrong ip format\n");
+			exit(1);
+		}
+
 	} else {
-		printf("Usage: check_pacp <file>\n");
+		usage();
 		exit(1);
 	}
 
